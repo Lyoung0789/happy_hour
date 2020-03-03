ea049dbc17ac97920731a4c15376d98be39ef285
Lester Young <lyoung070789@gmail.com>
2020-03-03 04:53:49 +0000

    updated CLI with excceptions

--- lib/happy_hour/cli.rb
+++ lib/happy_hour/cli.rb
@@ -1,14 +1,20 @@
 class HappyHour::CLI 
   
   def call
+    zip_code
     list_names
     information
     the_end
   end 
   
-  def list_names 
-    puts "Hi! Enter your zip code to find restarunts offering Happy Hour near you:"
+  
+  def zip_code
+     puts "Hi! Enter your zip code to find restarunts offering Happy Hour near you:"
     input = gets.strip
+  end 
+  
+  #list_names will need to tak in argument from zipcode
+  def list_names 
     puts "1. Applebees"
     puts "2. Domu"
     puts "3. Tori Tori"
@@ -17,8 +23,8 @@ class HappyHour::CLI
   
   def information 
     input = " "
-    puts "Enter the number of the restaruant which you would like to view more details or type exit to go back to menu" 
     while input != "exit"
+    puts "Enter the number of the restaruant which you would like to view more details or type list to view list again or type exit to go back to menu" 
       input = gets.strip.downcase
       case input 
         when "1"
@@ -27,6 +33,10 @@ class HappyHour::CLI
           puts "youve accessed more info on Domutype exit to end program"
         when "3"
           puts "youve accessed more info on Tori Tori type exit to end program"
+        when "list"
+          list_names
+        else 
+          puts "Please enter valid selection"
       end 
     end 
   end 
